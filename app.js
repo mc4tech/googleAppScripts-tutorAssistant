4b2dc163149ec890e96c6e063495ee38210486b1
Meguel Chavez <chavezmeguel@gmail.com>
2022-02-01 20:45:25 -0500

    working email confirmations

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
+const {localeOptions, gmailOpts} = require('./config')
 // Gets data from Student Roster
 function getStudentRoster() {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet();
+  const activeSheet = SpreadsheetApp.getActiveSpreadsheet();
   // const sheetName = sheet.getName()
+  const sheet = activeSheet.getSheetByName("Student Roster");
   const headers = sheet.getRange("A2:G2").getValues()[0];
   // const table = sheet.getDataRange().getValues()
-  const table = sheet
-    .getSheetByName("Student Roster")
-    .getDataRange()
-    .getValues();
+  const table = sheet.getDataRange().getValues();
   const data = {
     headers,
     table,
@@ -25,18 +24,6 @@ function isString(str) {
 // get all tutor sessions for tomorrow
 function getTutorSessions(roster) {
   const { headers, table } = roster;
-
-  const localeOptions = {
-    lang: "en-US",
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true,
-    weekDay: "long",
-    month: "long",
-    day: "2-digit",
-    year: "numeric",
-    defaultTimeZone: "EST",
-  };
 
   const emailColIndex = headers.findIndex((el) => {
     if (isString(el)) {
@@ -56,8 +43,14 @@ function getTutorSessions(roster) {
     }
     return false;
   });
+  const zoomIndex = headers.findIndex((el) => {
+    if (isString(el)) {
+      return el.toLowerCase().includes("zoom");
+    }
+    return false;
+  });
   const tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 3);
+  tomorrow.setDate(tomorrow.getDate() + 1);
 
   const tutorSessions = [];
   const events = CalendarApp.getEventsForDay(tomorrow);
@@ -70,6 +63,10 @@ function getTutorSessions(roster) {
 
     if (!isCanceled && isTutorSession) {
       const myStatus = el.getMyStatus();
+      const titleArr = title.split(" ");
+      const nameIndex =
+        titleArr.findIndex((el) => el.toLowerCase() === "and") + 1;
+      const tutorName = `${titleArr[nameIndex]} ${titleArr[nameIndex + 1]}`;
       const guestStatus = el.getGuestList()[0]
         ? el.getGuestList()[0].getStatus()
         : null;
@@ -92,12 +89,14 @@ function getTutorSessions(roster) {
         guestStatus,
         guestEmail,
         sessionDate,
+        tutorName,
       };
 
       const student = table.find(
         (row) => row[emailColIndex] === session.guestEmail
       );
       const studentName = student[studentNameIndex].split(", ");
+      const zoomLink = student[zoomIndex];
       const timeZone = student[timeZoneColIndex]
         ? student[timeZoneColIndex]
         : "";
@@ -116,12 +115,16 @@ function getTutorSessions(roster) {
       const firstName = studentName[1];
       const lastName = studentName[0];
 
-      session.firstName = firstName;
-      session.lastName = lastName;
+      session.studentFirstName = firstName;
+      session.studentLastName = lastName;
       session.hasTimeZone = hasTimeZone;
       session.sessionStart = sessionStart;
-      session.timeZone = timeZone;
       session.defaultTimeZone = localeOptions.defaultTimeZone;
+      session.timeZone = timeZone;
+      session.zoomLink = zoomLink;
+      session.formattedStart = `${session.sessionDate} ${
+        session.sessionStart
+      } ${session.hasTimeZone ? session.timeZone : session.defaultTimeZone}`;
 
       console.log(session);
       tutorSessions.push(session);
@@ -137,50 +140,103 @@ async function filterTutorSessions() {
   return tutorSessions;
 }
 
-//   function getMonday(d) {
-//   d = new Date(d);
-//   var day = d.getDay(),
-//       diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
-
-//   const dateRange = {
-//     currentMonday: new Date(d.setDate(diff)),
-//     currentSunday: new Date(d.setDate(diff +6)),
-//     prevMonday: new Date(d.setDate(diff -7)),
-//     prevSunday: new Date(d.setDate(diff -1))
-//   }
-//   return dateRange;
-// }
-
 async function buildEmails() {
   // const emailApp = GmailApp.
-  const templateNames = {
-    introEmail: "Intro",
-    confirmationEmail: "Confirmation",
-    weekendEmail: "Weekend",
-  };
-
-  const email = {
+  const data = {
     tutorSessions: await filterTutorSessions(),
     templateEmail: await getTemplate(templateNames.confirmationEmail),
   };
-  const emailApp = GmailApp.sendEmail(
-    "chavezmeguel@yahoo.com",
-    email.templateEmail.subject,
-    "testing html body",
-    { htmlBody: email.templateEmail.body }
-  );
+  const { tutorSessions, templateEmail } = data;
+
+  const completedTemplates = fillTemplate(tutorSessions, templateEmail);
+  const emailApp = GmailApp;
+  completedTemplates.forEach((template) => {
+    emailApp.sendEmail(
+      template.studentEmail,
+      template.subject,
+      template.body,
+      {
+        htmlBody: template.htmlBody,
+        cc: gmailOpts.ccTo,
+        // bcc: template.bcc
+      }
+    );
+  });
 }
 
 function getTemplate(templateType) {
   const drafts = GmailApp.getDrafts();
   const messages = drafts.map((el) => el.getMessage());
 
-  const findTemplate = messages.find((el) => el.getSubject().includes(templateType) );
+  const findTemplate = messages.find((el) =>
+    el.getSubject().includes(templateType)
+  );
   const templateEmail = {
     subject: findTemplate.getSubject(),
-    body: findTemplate.getBody(),
-    // messageId: findTemplate.getMessageId(),
+    htmlBody: findTemplate.getBody(),
+    body: findTemplate.getPlainBody(),
     id: findTemplate.getId(),
   };
   return templateEmail;
+}
+
+function fillTemplate(tutorSessions, templateEmail) {
+  const placeHolders = {
+    studentFirstName: {
+      temp: "<Student First Name>",
+      body: true,
+    },
+    bodyDate: {
+      temp: "<Day of week / Date and Time Using The Student’s Timezone><",
+      secondTemp: "Specify Timezone>",
+      secondVal: "",
+      body: true,
+    },
+    subjectDate: {
+      temp: "<Day of week / Date and Time Using The Student’s Timezone><Specify Timezone>.",
+      subject: true,
+    },
+    zoomLink: {
+      temp: "<Tutor’s Zoom Link>",
+      body: true,
+    },
+    tutorName: {
+      temp: "<Tutor First Name>",
+      body: true,
+    },
+    curriculum: {
+      temp: "<Curriculum>",
+      subject: true,
+    },
+  };
+  const templates = tutorSessions.map((session) => {
+    let { subject, body } = templateEmail;
+    session.curriculum = "FSF";
+    for (let key in placeHolders) {
+      const replaceThis = placeHolders[key].temp;
+      const re = new RegExp(replaceThis, "gi");
+      const newValue = key.includes("Date")
+        ? session.formattedStart
+        : session[key];
+      if (placeHolders[key].subject) {
+        subject = subject.replace(re, newValue);
+      }
+      if (placeHolders[key].body) {
+        if (placeHolders[key].secondTemp) {
+          const replaceSec = placeHolders[key].secondTemp;
+          const secRE = new RegExp(replaceSec, "gi");
+          const secValue = placeHolders[key].secondVal;
+
+          body = body.replace(re, newValue).replace(secRE, secValue);
+        } else {
+          body = body.replace(re, newValue);
+        }
+      }
+    }
+    const studentEmail = session.guestEmail;
+    const filledTemplate = { subject, body, studentEmail };
+    return filledTemplate;
+  });
+
+  return templates;
 }
---
+++ exampleConfig.js
@@ -0,0 +1,18 @@
+const localeOptions = {
+    lang: "String here",
+    hour: "String here",
+    minute: "String here",
+    hour12: "Boolean here",
+    weekDay: "String here",
+    month: "String here",
+    day: "String here",
+    year: "String here",
+    defaultTimeZone: "String here",
+  };
+
+  const gmailOpts = {
+      ccTo: "String here"
+  }
+
+
+  module.exports = {localeOptions,gmailOpts}
